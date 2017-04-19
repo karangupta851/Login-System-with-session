08a7cd21a07e439c8d54101ae9f1fbad57442924
Karan Gupta <karangupta851@gmail.com>
2017-04-20 00:10:36 +0530

    Update adminregistration.php

--- adminregistration.php
+++ adminregistration.php
@@ -35,11 +35,6 @@
             echo "<div class='form'><h3>You are registered successfully.</h3><br/>Click here to <a href='index.php'>Login</a></div>";
         }
     }else{
-	    //this will show you the error 
-            printf("%s\n", $con->error);
-            exit();
-        }
-    }else{
 ?>
 <div class="logo">
 <img src="image/logo3.png" />
