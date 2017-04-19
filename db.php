08d95d718085294818098620b2dc91cf2f40015b
Karan Gupta <karangupta851@gmail.com>
2017-04-19 23:23:19 +0530

    Update db.php

--- db.php
+++ db.php
@@ -4,10 +4,10 @@ $port = getenv('port');
 $username = getenv('username');
 $password = getenv('password');
 $database = getenv('database');
-$con = mysqli_connect($host, $username, $password, $database);
+$con = new mysqli($host, $username, $password, $database);
 // Check connection
 if (mysqli_connect_errno())
 {
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 ?>
