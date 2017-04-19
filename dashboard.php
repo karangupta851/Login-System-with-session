70844e830b8b59cfbd1cf899cae60a388641136b
Karan Gupta <karangupta851@gmail.com>
2017-04-20 01:01:57 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -10,7 +10,7 @@ include("session.php"); //include session.php file on all secure pages ?>
 </head>
 <body>
 <div class="main_div_mid">
-<p>Welcome <?php echo $_SESSION['first_name']; ?>!</p>
+<p>Welcome <?php echo $_SESSION['email']; ?>!</p>
 <p>Dashboard</p>
 <a href="logout.php">Logout</a>
 </div>
