d117780801d24069071bdc93c99e84ebf09abe1a
Karan Gupta <karangupta851@gmail.com>
2017-04-20 16:19:28 +0530

    Update staffregistration.php

--- staffregistration.php
+++ staffregistration.php
@@ -33,7 +33,7 @@
 		$salary = mysqli_real_escape_string($con,$salary);
 	    	$roles_id = stripslashes($_REQUEST['roles_id']);
 		$roles_id = mysqli_real_escape_string($con,$roles_id);
-        $query = "INSERT into `users` (first_name, last_name, password, email, address, adhaar_no, mobile, gender, dob, salary, roles_id) VALUES ('$first_name', 'last_name', '".md5($password)."', '$email', '$address', '$adhaar_no', '$mobile', '$gender', '$dob', '$salary', '$roles_id')";
+        $query = "INSERT into `users` (first_name, last_name, password, email, address, adhaar_no, mobile, gender, dob, salary, roles_id) VALUES ('$first_name', '$last_name', '".md5($password)."', '$email', '$address', '$adhaar_no', '$mobile', '$gender', '$dob', '$salary', '$roles_id')";
         $result = $con->query($query);
         if($result){
             echo "<div class='form'><h3>You are registered successfully.</h3><br/>Click here to <a href='index.php'>Login</a></div>";
