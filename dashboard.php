943c2e137cef5d8d091dd4a8f308b37ccfda37d5
Karan Gupta <karangupta851@gmail.com>
2017-04-23 15:32:37 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -23,23 +23,23 @@ include("session.php"); //include session.php file on all secure pages ?>
 </div>
 <table width="1057" class="top-links">
   <tr>
-<td width="154">
+<td width="165">
 <a href="dashboard.php">
 <h3>
 <font color="#FFFFFF" style="padding-top: 20px;padding-bottom: 20px;padding-left: 20px;padding-right: 20px;">Dashboard</font></h3></a></td>
-<td width="154">
+<td width="165">
 <a href="adddegree.php">
 <h3><font color="#FFFFFF" style="padding-top: 20px;padding-bottom: 20px;padding-left: 20px;padding-right: 20px;">Add Degree</font></h3></a></td>
-<td width="154">
+<td width="165">
 <a href="addcourse.php">
 <h3><font color="#FFFFFF" style="padding-top: 20px;padding-bottom: 20px;padding-left: 20px;padding-right: 20px;">Add Course</font></h3></a></td>
-<td width="154">
+<td width="165">
 <a href="subject.php"><h3>
-<font color="#FFFFFF" style="padding-top: 20px;padding-bottom: 20px;padding-left: 20px;padding-right: 20px;">Add Subject</font></h3></td>
-<td width="154">
+<font color="#FFFFFF" style="padding-top: 20px;padding-bottom: 20px;padding-left: 20px;padding-right: 20px;">Add Subject</font></h3></a></td>
+<td width="165">
 <font color="#FFFFFF" style="padding-top: 20px;padding-bottom: 20px;padding-left: 20px;padding-right: 20px;">
 <?php echo $_SESSION['email']; ?>!</font></td>
-<td width="452">
+<td width="165">
 <a href="logout.php">
 <h3><font color="#FFFFFF" style="padding-top: 20px;padding-bottom: 20px;padding-left: 20px;padding-right: 20px;">Logout</font></h3></a></td></tr>
 </table>
