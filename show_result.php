e5ea45f4d1cf073e033c610dea1d08796115e5d0
Karan Gupta <karangupta851@gmail.com>
2017-04-23 19:07:55 +0530

    Add files via upload

---
+++ show_result.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Welcome Home</title>
+<link rel="stylesheet" href="CSS/style.css" />
+</head>
+<body>
+
+<div id="main">
+</div>
+<div class="main_div_mid">
+</div>
+<div class="main_div_left">
+</div>
+<div class="logo">
+<img src="image/logo3.png" />
+</div>
+<div class="ums_logo">
+<img src="image/ums_logo.png" />
+</div>
+<div class="login_box" style="margin-left: 370px;">
+<div class="red_box">
+</div>
+<div class="sign_in">Show Result
+</div>
+<form action="" method="post" name="registration">
+<div class="table">
+<div id="UpdatePanel1">
+  <table align="center" width="100%" border="0" cellspacing="30" cellpadding="0" class="login_tbl">
+    <tr>
+      <td height="41"><h3>Student Roll No. </h3></td>
+      <td><input name="student_id" type="text" class="input_type" id="student_id" placeholder="Student Roll" required="required" /></td>
+    </tr>
+
+    <tr>
+      <td><div class="login_button">
+          <input type="image" name="register" id="register" src="image/show-button-png-hi.png" style="height:80px;width:130px;border-width:0px; " />
+      </div></td>
+    </tr>
+  </table>
+</div>
+</div>
+</form></div>
+<?php } ?>
+</body>
+</html>
