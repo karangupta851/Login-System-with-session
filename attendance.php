e16703ec0ffbc530e49275d1ab7d0614b237547e
Tarun Gupta <soul.tarun@gmail.com>
2017-04-23 23:37:32 +0530

    attendance of mulitpel students added

--- attendance.php
+++ attendance.php
@@ -8,25 +8,55 @@
 <body>
 <?php
 	require('db.php');
+
+    $getStudents = "select * from users where roles_id=3";
+    $result = $con->query($getStudents);
+    printf("%s\n", $con->error);
+    $rows = $result->fetch_array();
+    $len = count($rows);
+    echo "<pre>";
+
+    $students = [];
+    for ($i=0; $i < $len; $i++) { 
+      $row = $result->fetch_array(MYSQLI_ASSOC);
+      if($row)
+        $students[] = $row;
+    }
+
+    // print_r($students);
+    // die;
+
     // If form submitted, insert values into the database.
-    if (isset($_REQUEST['student_id'])){
-		$student_id = stripslashes($_REQUEST['student_id']); // removes backslashes
-		$student_id = mysqli_real_escape_string($con,$student_id); //escapes special characters in a string
+    if (isset($_POST) && !empty($_POST)){
+      print_r($_POST);
+		// $student_id = stripslashes($_REQUEST['student_id']); // removes backslashes
+		// $student_id = mysqli_real_escape_string($con,$student_id); //escapes special characters in a string
 		$subjects_id = stripslashes($_REQUEST['subjects_id']);
 		$subjects_id = mysqli_real_escape_string($con,$subjects_id);
 		$classes_delivered = stripslashes($_REQUEST['classes_delivered']);
 		$classes_delivered = mysqli_real_escape_string($con,$classes_delivered);
 		$classes_attended = stripslashes($_REQUEST['classes_attended']);
 		$classes_attended = mysqli_real_escape_string($con,$classes_attended);
-        $query = "INSERT into `student_attendance` (student_id, subjects_id, classes_delivered, classes_attended) VALUES ('$student_id', '$subjects_id', '$classes_delivered', '$classes_attended')";
-        $result = $con->query($query);
-        if($result){
-            echo "<div class='form'><h3>course registered successfully.</h3><br/>Click here to <a href='dashboard.php'>Dashboard</a></div>";
-        }else{
-            //this will show you the error 
-            printf("%s\n", $con->error);
-            exit();
-        }
+    $query = "";
+    // print_r($_POST['student']);
+    foreach ($_POST['student'] as $key => $s) {
+
+      // echo $s;
+      $query .= "INSERT into `student_attendance` (student_id, subjects_id, classes_delivered, classes_attended) VALUES ('$s', '$subjects_id', '$classes_delivered', '$classes_attended');";
+    }
+      if ($con->multi_query($query) === TRUE) {
+          echo "New records created successfully";
+      } else {
+          echo "Error: " . $query . "<br>" . $con->error;
+      }
+        // $result = $con->query($query);
+        // if($result){
+        //     echo "<div class='form'><h3>course registered successfully.</h3><br/>Click here to <a href='dashboard.php'>Dashboard</a></div>";
+        // }else{
+        //     //this will show you the error 
+        //     printf("%s\n", $con->error);
+        //     exit();
+        // }
     }else{
 ?>
 <div id="main">
@@ -50,10 +80,10 @@
 <div class="table">
 <div id="UpdatePanel1">
   <table align="center" width="100%" border="0" cellspacing="30" cellpadding="0" class="login_tbl">
-  <tr>
+  <!-- <tr>
       <td height="41"><h3>Student Roll No. </h3></td>
       <td><input name="student_id" type="text" class="input_type" id="name" placeholder="Student Name" required="required" /></td>
-    </tr>
+    </tr> -->
     <tr>
       <td height="41"><h3>Subject Name</h3></td>
       <td><select name="subjects_id" class="select_ums" id="subjects_id">
@@ -74,12 +104,33 @@
       <td height="41"><h3>Class Attended </h3></td>
       <td><input name="classes_attended" type="text" class="input_type" id="name" placeholder="Class Attended" required="required" /></td>
     </tr>
-	  <tr>
-  <td><div class="login_button">
-    <input type="image" name="register" id="register" src="add-button-blue-hi.png" alt="register" style="height:80px;width:130px;border-width:0px; " />
-  </div></td>
-  </tr>
-  </table>
+	  
+  
+</table>
+  <table align="center" width="100%" border="0" cellspacing="30" cellpadding="0" class="login_tbl">
+    <tr>
+      <th></th>
+      <th>Name</th>
+      <th>Last Name</th>
+      <th>Email</th>
+    </tr>
+    <?php 
+      foreach ($students as $key => $student) {
+        // print_r($student);
+        
+        echo "<tr>";
+        echo "<td><input type='checkbox' name='student[]' value='".$student['id']."' /> </td>";
+        echo "<td>".$student['first_name'].$student['id']."</td>";
+        echo "<td>".$student["last_name"]."</td>";
+        echo "<td>".$student['email']." </td>";
+        echo "</tr>";
+      }
+    ?>
+  <table>  
+  <div class="login_button">
+        <input type="image" name="register" id="register" src="add-button-blue-hi.png" alt="register" style="height:80px;width:130px;border-width:0px; " />
+      </div>
+  
   </div>
 </div>
 </form></div>
