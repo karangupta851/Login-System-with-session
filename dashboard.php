6d98c54772a6c8432e146a79b52080b54dd3ee0d
Karan Gupta <karangupta851@gmail.com>
2017-04-23 18:55:23 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -21,25 +21,40 @@ include("session.php"); //include session.php file on all secure pages ?>
 <div class="ums_logo">
 <img src="image/ums_logo.png" />
 </div>
-<table width="1057" class="top-links">
+<table bgcolor="#3399CC"width="1350" class="top-links">
   <tr>
-<td width="165">
+<td width="119" height="32">
 <a href="dashboard.php">
 <h3>
 <font color="#FFFFFF" style="padding-top: 20px;padding-bottom: 20px;padding-left: 20px;padding-right: 20px;">Dashboard</font></h3></a></td>
-<td width="165">
+<td width="135">
 <a href="adddegree.php">
-<h3><font color="#FFFFFF" style="padding-top: 20px;padding-bottom: 20px;padding-left: 20px;padding-right: 20px;">Add Degree</font></h3></a></td>
-<td width="165">
-<a href="addcourse.php">
-<h3><font color="#FFFFFF" style="padding-top: 20px;padding-bottom: 20px;padding-left: 20px;padding-right: 20px;">Add Course</font></h3></a></td>
-<td width="165">
-<a href="subject.php"><h3>
+<h3><font color="#FFFFFF" style="padding-top: 20px;padding-bottom: 20px;padding-left: 20px;padding-right: 20px;">Add Degree</font></h3>
+</a></td>
+<td width="135"><a href="addcourse.php">
+  <h3><font color="#FFFFFF" style="padding-top: 20px;padding-bottom: 20px;padding-left: 20px;padding-right: 20px;">Add Course</font></h3>
+</a></td>
+<td width="137">
+<a href="subject.php"></a>
+<h3>
+<a href="subject.php"><font color="#FFFFFF" style="padding-top: 20px;padding-bottom: 20px;padding-left: 20px;padding-right: 20px;">Add Subject</font></a></h3>
+<a href="subject.php"></a></td>
+<td width="167">
+<h3>
+<a href="show_result.php"><font color="#FFFFFF" style="padding-top: 20px;padding-bottom: 20px;padding-left: 20px;padding-right: 20px;">Check Result</font></a></h3></td>
+<td width="150">
+<a href="subject.php"> 
+<h3>
 <font color="#FFFFFF" style="padding-top: 20px;padding-bottom: 20px;padding-left: 20px;padding-right: 20px;">Add Subject</font></h3></a></td>
-<td width="165">
+<td width="136">
+<a href=""> 
+<h3>
+<font color="#FFFFFF" style="padding-top: 20px;padding-bottom: 20px;padding-left: 20px;padding-right: 20px;">Add result</font></h3></a></td>
+<td width="35"></td>
+<td width="157">
 <font color="#FFFFFF" style="padding-top: 20px;padding-bottom: 20px;padding-left: 20px;padding-right: 20px;">
 <?php echo $_SESSION['email']; ?>!</font></td>
-<td width="165">
+<td width="140">
 <a href="logout.php">
 <h3><font color="#FFFFFF" style="padding-top: 20px;padding-bottom: 20px;padding-left: 20px;padding-right: 20px;">Logout</font></h3></a></td></tr>
 </table>
